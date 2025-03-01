fd563e3064b5ea52f4e482b84f68fc1c8e46ea0f
Antonio <antonioangosoo@gmail.com>
2025-03-01 12:05:09 +0100

    Primer commit Practicas Lenguajes

---
+++ PracticasLenguajesGit/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PracticasLenguajesGit
+//
+//  Created by Toni on 28/2/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
